50a6cebcc72140a85ea8450f1b32efc5d907a5df
boo.jm <boo.jm@daum.net>
2018-12-19 07:08:20 +0900

    과제 완료

---
+++ src/main/java/boojongmin/localsearch/controller/LocalSearchController.java
@@ -0,0 +1,36 @@
+package boojongmin.localsearch.controller;
+
+import boojongmin.localsearch.model.DocumentModel;
+import boojongmin.localsearch.model.KeywordCountModel;
+import boojongmin.localsearch.model.LocationSearchModel;
+import boojongmin.localsearch.service.LocalSearchService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/localsearch")
+public class LocalSearchController {
+    private LocalSearchService service;
+
+    public LocalSearchController(LocalSearchService service) {
+        this.service = service;
+    }
+
+
+    // ex: http://localhost:8080/api/localsearch/keyword/1/page/1
+    @GetMapping({"/keyword/{keyword}" , "/keyword/{keyword}/page/{page}"})
+    public LocationSearchModel locationSearch(@PathVariable("keyword") String keyword,
+                                              @PathVariable(name="page", required = false) Optional<Integer> pageOpt) {
+        return service.locationSearch(keyword, pageOpt);
+    }
+
+    @GetMapping("/keyword/rank")
+    public List<KeywordCountModel> keywordRank() {
+        return service.getKeywordCount();
+    }
+}
